e67f56397b80ecbb7861f6a0bd972fc43c8aab94
Yhprumenahs <shanomac1983@gmail.com>
2024-05-15 19:14:07 +0000

    lab1 code works
    Signed-off-by: Yhprumenahs <shanomac1983@gmail.com>

--- main.cpp
+++ main.cpp
@@ -4,21 +4,27 @@
 
  InterruptIn buttn(PC_13);
 
+ volatile int myflag = 0;
+
  void buttn_fall(){
-        printf("button pressed!!\r\n");
+     myflag = 1;
     
  }
 
 // main() runs in its own thread in the OS
 int main()
 {
-    printf("This is mbed os v%d \r\n", MBED_VERSION);
-
+    printf("this is mbed os v%d \r\n", MBED_VERSION);
+    //turns on and prints what mbed version the mbed is
     buttn.fall(&buttn_fall);
     while (true) {
-        
+        if(myflag == 1){
+            printf("button pressed!!\r\n");
+            //button is pressed and 'button pressed' appears on coolterm
+            myflag = 0;
+            //when button is not pressed the flag resets to zero and nothings is printed until the button is pressed again
+        }
 
     }
 }
-
 
